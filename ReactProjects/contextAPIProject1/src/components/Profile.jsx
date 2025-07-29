578efa743e079905ba0a8a52399d1134740de615
macxbeast <de4881mimansa@gmail.com>
2025-07-29 08:40:11 +0530

    completed contexAPI project1

---
+++ ReactProjects/contextAPIProject1/src/components/Profile.jsx
@@ -0,0 +1,10 @@
+import React,{useContext} from "react";
+import UserContext from "../context/UserContext";
+
+function Profile(){
+    const {user}=useContext(UserContext)
+    if(!user) return <div>Please Login First</div>
+    return <div>Welcome {user.username}, your password is {user.password}</div>
+}
+
+export default Profile
