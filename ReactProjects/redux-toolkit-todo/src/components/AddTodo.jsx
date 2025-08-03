519f09c40484fcbcf4027cc4893e54fd91113593
macxbeast <de4881mimansa@gmail.com>
2025-08-03 14:22:27 +0530

    Changes made to todo using redex toolkit

--- ReactProjects/redux-toolkit-todo/src/components/AddTodo.jsx
+++ ReactProjects/redux-toolkit-todo/src/components/AddTodo.jsx
@@ -1,19 +1,38 @@
-import React, { useState } from "react";
-import {useDispatch} from 'react-redux'
+import React, { useEffect, useState } from "react";
+import {useDispatch,useSelector} from 'react-redux'
 import {addTodo} from '../features/todo/todoSlice'
+import { editTodo,finalUpdate } from "../features/todo/todoSlice";
 
 function AddTodo (){
     const [input,setInput]=useState("")
     const dispatch=useDispatch()
+    const editText=useSelector(state => state.edit)
 
-    const addTodoHandler = (e)=>{
-        e.preventDefault();
-        dispatch(addTodo(input))
-        setInput('')
+    const handleSubmit = (e)=>{
+      e.preventDefault();
+      if(editText.length===0){
+        if(input)dispatch(addTodo(input))
+      }
+      else{
+        if(input) dispatch(finalUpdate(input))
+      }
+      setInput("")
     }
 
+    useEffect(()=>{
+      if(editText.length!==0){
+        setInput(editText[1])
+      }
+      else{
+        setInput("")
+      }
+    },[editText])
+        
+
+    
+
     return (
-    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+    <form onSubmit={handleSubmit} className="space-x-3 mt-12">
       <input
         type="text"
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
@@ -25,7 +44,7 @@ function AddTodo (){
         type="submit"
         className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
       >
-        Add Todo
+        {editText.length===0?"Add Todo":"Update Todo"}
       </button>
     </form>
     )
--- ReactProjects/redux-toolkit-todo/src/features/todo/todoSlice.js
+++ ReactProjects/redux-toolkit-todo/src/features/todo/todoSlice.js
@@ -1,7 +1,8 @@
 import {createSlice,nanoid} from "@reduxjs/toolkit" 
 
 const initialState = {
-    todos:[]
+    todos:[],
+    edit: []
 }
 
 export const todoSlice = createSlice({
@@ -13,14 +14,27 @@ export const todoSlice = createSlice({
                 id: nanoid(),
                 text: action.payload
             }
-            state.todos.push(todo)
+            state.todos=[todo, ...state.todos]
         },
         removeTodo: (state,action)=>{
             state.todos = state.todos.filter((todo) => todo.id!==action.payload)
+            state.edit=[]
+        },
+        updateTodo: (state,action)=>{
+            state.todos = state.todos.map((todo)=> (todo.id===action.payload[0])?{...todo,text:action.payload[1]}:todo)
+        },
+        editTodo:(state,action)=>{
+            state.edit=[action.payload[0],action.payload[1]]
+            
+        },
+        finalUpdate:(state,action)=>{
+            state.todos = state.todos.map((todo)=> (todo.id===state.edit[0])?{...todo,text:action.payload}:todo)
+            state.edit=[]
         }
+
     }
 })
 
-export const {addTodo,removeTodo} = todoSlice.actions
+export const {addTodo,removeTodo,updateTodo,editTodo,finalUpdate} = todoSlice.actions
 
 export default todoSlice.reducer
